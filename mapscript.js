66c14a48b2e65c76babdfd1d3b881750ce054a3c
Elizabeth Joseph <744248@student.tor.loc>
2019-04-11 11:02:31 -0400

    add script to Map DOM Element page

--- mapscript.js
+++ mapscript.js
@@ -1,4 +1,4 @@
-var map1,map2,map3;
+var map,map1,map2,map3;
 function initialize(){
     var mapOptions1={
         zoom:2,
@@ -12,9 +12,14 @@ function initialize(){
         zoom:18,
         center:{lat:40.758904,lng:-73.985128}
     };
+    var mapOptions={
+        zoom:12,
+        center:{lat:40.758904,lng:-73.985128}
+    };
 
     map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
     map2=new google.maps.Map(document.getElementById('map_canvas2'),mapOptions2);
     map3=new google.maps.Map(document.getElementById('map_canvas3'),mapOptions3);
+    map=new google.maps.Map(document.getElementById('map_canvas'),mapOptions);
 }
 google.maps.event.addDomListener(window,'load',initialize);
